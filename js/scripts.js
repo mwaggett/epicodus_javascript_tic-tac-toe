86d037de64b14808be2400e7766917f1c9f10239
Molly Waggett & Yelena Belikova <student@epicodus.com>
2015-09-23 09:43:23 -0700

    Create Player, Space, and Board objects; sample specs from assignment page all met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-// var functionName = function(params) {
-//
-// };
+function Player(mark) {
+  this.mark = mark;
+}
+
+function Space(x_coordinate, y_coordinate) {
+  this.x_coordinate = x_coordinate;
+  this.y_coordinate = y_coordinate;
+}
+
+Space.prototype.mark_by = function(player) {
+  this.markedBy = player;
+}
+
+function Board() {
+  this.spaces = [new Space(1,1), new Space(1,2), new Space(1,3), new Space(2,1), new Space(2,2), new Space(2,3), new Space(3,1), new Space(3,2), new Space(3,3) ]
+}
+
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,33 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('Player', function() {
+  it("returns the player's mark", function() {
+    var testPlayer = new Player("X");
+    expect(testPlayer.mark).to.equal("X");
+  });
+});
+
+describe('Space', function() {
+  it("returns coordinates of Space instance", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.x_coordinate).to.equal(1);
+    expect(testSpace.y_coordinate).to.equal(2);
+  });
+
+  it("lets a player mark a space", function() {
+    var testPlayer = new Player("X")
+    var testSpace = new Space(1,2);
+    testSpace.mark_by(testPlayer)
+    expect(testSpace.markedBy).to.equal(testPlayer);
+  });
+
+  it("returns undefined when not marked", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.markedBy).to.equal(undefined);
+  });
+});
+
+describe('Board', function() {
+  it("creates 9 spaces when it is initialized", function() {
+    var testBoard = new Board();
+    expect(testBoard.spaces[4].x_coordinate).to.equal(2);
+   });
+});
